c1e4bbbb03c0fe055d7a38f0a06629e5d84b088f
booty <booty.lin@Softtek.com>
2023-03-22 14:11:33 +0800

    dev

---
+++ Git/git.js
@@ -0,0 +1,23 @@
+/*
+
+远程git地址
+https://github.com/lbylattice/Study_Front_End
+
+
+git配置使用 HTTP/1.1
+git config --global http.version HTTP/1.1
+
+git配置使用 HTTP/2
+git config --global http.version HTTP/2
+
+
+git配置
+查看本地配置(你可能会看到重复的变量名，因为 Git 会从不同的文件中读取同一个配置)
+git config --list --show-origin
+git config --list
+
+修改git配置
+user.name "John Doe"
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+*/
